9b327c9480402c01f25160817defe5179f7f7e44
XenoAmess <xenoamess@gmail.com>
2019-06-12 19:41:52 +0800

    version to 0.8.1

---
+++ src/main/java/com/xenoamess/commonx_additional_tests/Generator.java
@@ -0,0 +1,10 @@
+package com.xenoamess.commonx_additional_tests;
+
+/**
+ * @author XenoAmess
+ */
+public class Generator {
+    public static void main(String[] args) {
+        com.xenoamess.commons.code_generators.GeneratePrimitivesFromDouble.main(args);
+    }
+}
